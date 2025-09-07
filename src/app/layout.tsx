6604859b8abf07e4152d178d4009631679ef52bc
abdellah <Abdellah.chehri14@gmail.com>
2025-09-07 22:29:46 +0200

    feat: add seo metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,34 @@ const montserrat = Montserrat({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Jethings",
-  description: "Stack managment software",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Jethings",
+    description: "Stock management software",
+    openGraph: {
+      title: "Jethings",
+      description: "Stock management software",
+      url: "https://jethings.com",
+      siteName: "Jethings",
+      images: [
+        {
+          url: "/jethings-openghraf.png",
+          width: 1200,
+          height: 630,
+          alt: "Jethings - Stock management software",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Jethings",
+      description: "Stock management software",
+      images: ["/jethings-openghraf.png"],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
