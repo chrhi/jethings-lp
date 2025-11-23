e344de30a15a74d6091278b247380659d06c8341
Abdullah Chehri <116351398+chrhi@users.noreply.github.com>
2025-11-23 09:01:25 +0100

    feat: add sales links

---
+++ src/data/sales-links.ts
@@ -0,0 +1,41 @@
+export const SalersLinks = [
+  {
+    href: "/customers",
+    name: {
+      en: "customers",
+      fr: "customers",
+    },
+  },
+
+  {
+    href: "/customers-groups",
+    name: {
+      en: "customers groups",
+      fr: "customers groups",
+    },
+  },
+
+  {
+    href: "/sales-invoices",
+    name: {
+      en: "sales invoices",
+      fr: " customers orders",
+    },
+  },
+
+  {
+    href: "/price-lists",
+    name: {
+      en: "price list",
+      fr: "price list",
+    },
+  },
+
+  {
+    href: "/item-prices",
+    name: {
+      en: "item-prices",
+      fr: "item-prices",
+    },
+  },
+]
